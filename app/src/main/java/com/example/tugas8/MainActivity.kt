f458dafea5c46f58efc05453cc3f5c178858957a
udinkitaro29 <Udin.kitaro@gmail.com>
2023-11-17 15:11:28 +0700

    Ini Versi 1

---
+++ app/src/main/java/com/example/tugas8/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.tugas8
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tugas8.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mainBinding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+        mainBinding.btnSave.setOnClickListener{ goToProfilActivity() }
+    }
+    private fun goToProfilActivity() {
+        val intent = Intent(this, ProfilActivity::class.java)
+        startActivity(intent)
+
+    }
+}
+
---
+++ app/src/main/java/com/example/tugas8/ProfilActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tugas8
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfilActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profil)
+    }
+}
